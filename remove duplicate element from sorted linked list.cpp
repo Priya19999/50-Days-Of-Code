a1dd992d11a407967381a58d66b7ad072fe6daa5
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2020-12-29 17:40:30 +0530

    Add files via upload

---
+++ remove duplicate element from sorted linked list.cpp
@@ -0,0 +1,91 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// root: head node
+Node *removeDuplicates(Node *root)
+{
+ // your code goes here
+ struct Node *current=root;
+ struct Node *next_next; //it is to store the pinter of the node to be deleted
+
+ while(current!=NULL && current->next!=NULL)
+ {
+     if(current->data==current->next->data)
+     {
+         next_next=current->next->next;// storing the pointer to next node before deleting the next node
+         free(current->next);
+         current->next=next_next;
+         
+     }
+     else //only traverse if there is no deletion
+     {
+         current=current->next;
+     }
+    
+     
+ }
+  return root;
+}
