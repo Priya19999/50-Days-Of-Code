c3158233ada6edbf34665c0e37c5e3bf8f52387a
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2021-01-30 17:29:37 +0530

    Add files via upload

---
+++ lowest common ancestor.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL)
+        {
+            return NULL;
+        }
+        if(root==p || root==q)
+        {
+            return root;
+        }
+        TreeNode* left=lowestCommonAncestor(root->left,p,q); // p andq are the value that we will give
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if (left!=NULL && right!=NULL)
+        {
+            return root;
+        }
+         if (left==NULL && right==NULL)
+        {
+            return NULL;
+        }
+        if(left!=NULL)
+        {
+            return left;
+        }
+        else
+        {
+            return right;
+        }
+    }
+};
+
